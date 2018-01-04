ed7fe9c494581f80100135a0a057c1efd03631dd
Landers <luhaixing@qq.com>
2018-01-04 10:31:43 +0800

    ReportRunLogTrait中类型(type)改DIRECTORY 为 LOCAL_FILE

--- src/Traits/ReportRunLogTrait.php
+++ src/Traits/ReportRunLogTrait.php
@@ -1,7 +1,8 @@
 <?php
 namespace Landers\LaravelPlus\Traits;
 
-use Illuminate\Support\Str;
+use Illuminate\Support\Str as Str1;
+use Landers\Substrate2\Utils\Str as Str2;
 use Landers\LaravelPlus\Utils\Filesystem;
 
 trait ReportRunLogTrait
@@ -12,12 +13,11 @@ trait ReportRunLogTrait
      * 上报日志
      * @param array|string $data
      */
-    protected function reportLog( $data )
+    protected function reportLog( $data, $ways = [] )
     {
         $data = is_array($data) ? implode("\n", $data) : $data;
-        $data .= str_repeat(PHP_EOL, 3);
 
-        $ways = $this->getReportWays();
+        $ways or $ways = $this->getReportWays();
 
         array_walk($ways, function($type, $way) use (&$data) {
             try {
@@ -26,11 +26,8 @@ trait ReportRunLogTrait
                         $way($data);
                         break;
 
-                    case 'DIRECTORY':
-                        $app_name = strtolower(str_replace([':', '\\'], ['-', ''], Str::snake(static::class)));
-                        $today = date('Y-m-d');
-
-                        $filename = $way . ("/{$today}/{$app_name}.log");
+                    case 'LOCAL_FILE':
+                        $filename = $this->buildLogFilename($way);
                         app(Filesystem::class)->append($filename, $data, FILE_APPEND);
                 }
             } catch (\Exception $e) {
@@ -54,5 +51,37 @@ trait ReportRunLogTrait
         }
 
         return [];
+    }
+
+
+    /**
+     * 取得当前运行的应用程序
+     * @return string
+     */
+    private function getReportAppName()
+    {
+
+        $app_name = Str1::snake(static::class);
+        $app_name = str_replace([':', '\\'], ['-', ''], $app_name);
+        return $app_name;
+    }
+
+
+    /**
+     * 生成日志文件路径
+     * @param string $tpl_filename
+     * @return string
+     */
+    private function buildLogFilename( string $tpl_filename )
+    {
+        $replaces = [
+            'app_name' => $this->getReportAppName(),
+            'date' => date('Y-m-d'),
+            'sapi' => php_sapi_name(),
+        ];
+
+        $filename = Str2::replace($tpl_filename, $replaces);
+
+        return base_path($filename);
     }
 }
