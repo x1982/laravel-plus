dcf0816b0dcdd5ef5dede5adb35c9622d9453aa4
Landers <luhaixing@qq.com>
2018-02-09 15:33:54 +0800

    Cache 中新增针对验证类的缓存，如缓存最近10分趾短信验证码，最近1分钟的邮件中的token

--- src/Facades/Cache.php
+++ src/Facades/Cache.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Cache as LaravelCache;
 
 class Cache extends LaravelCache
 {
+    private static $verification_prefix_key = 'cache_verification';
+
     public static function compare( $cache_key, $value )
     {
         $cache_value = parent::get($cache_key);
@@ -13,5 +15,53 @@ class Cache extends LaravelCache
         } else {
             return false;
         }
+    }
+
+    /**
+     * 取得验证用的缓存
+     *
+     * @param $key
+     */
+    public static function getForVerification(string $key)
+    {
+        $key = self::$verification_prefix_key . '_' . $key;
+        return parent::get($key);
+    }
+
+    /**
+     * 设置验证用的缓存
+     *
+     * @param string $key
+     * @param $data
+     * @param $expireMinute
+     */
+    public static function putForVerification(string $key, $data, $expireMinute)
+    {
+        $key = self::$verification_prefix_key . '_' . $key;
+        return parent::set($key, $data, $expireMinute);
+    }
+
+    /**
+     * 清除验证用的缓存
+     *
+     * @param string $key
+     */
+    public static function forgetForVerification(string $key)
+    {
+        $key = self::$verification_prefix_key . '_' . $key;
+        return parent::forget($key);
+    }
+
+    /**
+     * 清除验证用的缓存
+     *
+     * @param string $key
+     * @param $data
+     * @param $expireMinute
+     */
+    public static function compareForVerification(string $key, $data)
+    {
+        $key = self::$verification_prefix_key . '_' . $key;
+        return self::compare($key, $data);
     }
 }
