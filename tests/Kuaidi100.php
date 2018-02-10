ac9638d55d2fa592b2d93208a11adf3ed06fa79c
Landers <luhaixing@qq.com>
2018-02-11 01:12:22 +0800

    更新测试

--- tests/Kuaidi100.php
+++ tests/Kuaidi100.php
@@ -2,9 +2,8 @@
 
 use Landers\LaravelAms\Constraints\Tests\BaseTestCase;
 use Landers\LaravelPlus\Supports\Kuaidi100\Kuaidi100Service;
-use Tests\TestCase;
 
-class Kuaidi100Test extends TestCase
+class Kuaidi100Test extends BaseTestCase
 {
     public function setUp()
     {
@@ -17,8 +16,13 @@ class Kuaidi100Test extends TestCase
      */
     public function testQueryType()
     {
-        $no = '611021231958194';
         $no = '472503282097';
-        dp(Kuaidi100Service::make()->query($no));
+        $result = Kuaidi100Service::make()->query($no);
+        $this->assertArrayHasKey('courier', $result);
+        $this->assertArrayHasKey('path', $result);
+
+        $no = '611021231958194';
+        $result = Kuaidi100Service::make()->query($no);
+        $this->assertFalse($result);
     }
 }
