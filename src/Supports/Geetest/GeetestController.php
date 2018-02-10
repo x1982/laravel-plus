0f8263625a75e69cec848b2be9cfa5b5eecac7a1
Landers <luhaixing@qq.com>
2018-02-10 22:16:46 +0800

    Components 即将改名为 Supports

---
+++ src/Supports/Geetest/GeetestController.php
@@ -0,0 +1,25 @@
+<?php
+namespace Landers\LaravelPlus\Supports\Geetest;
+
+use Landers\LaravelAms\Constraints\Controllers\BaseController;
+
+class GeetestController extends BaseController
+{
+
+    public function __construct(GeetestRepository $geetestRepository)
+    {
+        $this->repo = $geetestRepository;
+        parent::__construct();
+    }
+
+    /**
+     * 取得geetest验证码
+     *
+     * @return mixed
+     */
+    public function captcha()
+    {
+        return $this->repo->show();
+    }
+
+}
