b8070cbfefd8a375b7961a02d1de5076ca850385
Landers <luhaixing@qq.com>
2018-02-12 00:51:18 +0800

    生效 Landers\LaravelPlus\Supports ，废除  Landers\LaravelPlus\Components

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::group(['namespace' => 'Landers\LaravelPlus\Components'], function (){
+Route::group(['namespace' => 'Landers\LaravelPlus\Supports'], function (){
     //极验
     Route::group(['prefix' => 'geetest'], function () {
         Route::get('captcha', 'Geetest\GeetestController@captcha');
