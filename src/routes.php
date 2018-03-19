125201a3bc71d26d25ffdb659fa88d41f8fcd7ad
Landers <luhaixing@qq.com>
2018-03-20 00:55:43 +0800

    删除路由信息

--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,2 @@
 <?php
 use Illuminate\Support\Facades\Route;
-
-Route::group(['namespace' => 'Landers\LaravelPlus\Supports'], function (){
-    //极验
-    Route::group(['prefix' => 'geetest'], function () {
-        Route::get('captcha', 'Geetest\GeetestController@captcha');
-    });
-});
